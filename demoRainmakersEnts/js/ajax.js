4f5dc8130f302ba865e196092b01bd055b9b1fda
freelance <freelance-computing@outlook.com>
2020-04-23 00:20:09 +0100

    tried to be consistent with double quotes instead of single quotes; added input event listeners and additional validation for empty text fields

--- demoRainmakersEnts/js/ajax.js
+++ demoRainmakersEnts/js/ajax.js
@@ -1,35 +1,108 @@
 $(document).ready(function () {
 	// reset forms on page refresh
-	var contactForm = $('#contactForm');
+	var contactForm = $("#contactForm");
 	if (contactForm.length > 0) {
 		contactForm[0].reset();
 	}
-	var interestForm = $('#interestForm');
+	var interestForm = $("#interestForm");
 	if (interestForm.length > 0) {
 		interestForm[0].reset();
 	}
 });
 
 $(function () {
-	var form = $('#contactForm');
-	var nameError = $('#displayNameError');
+	var form = $("#contactForm");
+	if (form.length < 1) { return; }
+
+	// grab variables
+	var contactForm = form[0];
+	var nameError = $("#displayNameError");
+	var numberError = $("#displayNumberError");
+	var emailError = $("#displayEmailError");
+	var subjectError = $("#displaySubjectError");
+	var messageError = $("#displayMessageError");
+	var sendSuccess = $("#displaySuccess");
+
+	var addInputEvtListeners = function () {
+		contactForm.alias.addEventListener("input", function (event) {
+			if (contactForm.alias.validity.patternMismatch) {
+				nameError.css("display", "");
+				if (!form.hasClass("invalid")) {
+					form.addClass("invalid");
+				}
+			} else {
+				nameError.css("display", "none");
+				if (form.hasClass("invalid")) {
+					form.removeClass("invalid");
+				}
+			}
+		});
+		contactForm.number.addEventListener("input", function (event) {
+			if (contactForm.number.validity.patternMismatch) {
+				numberError.css("display", "");
+				if (!form.hasClass("invalid")) {
+					form.addClass("invalid");
+				}
+			} else {
+				numberError.css("display", "none");
+				if (form.hasClass("invalid")) {
+					form.removeClass("invalid");
+				}
+			}
+		});
+		contactForm.email.addEventListener("input", function (event) {
+			if (contactForm.email.validity.patternMismatch) {
+				emailError.css("display", "");
+				if (!form.hasClass("invalid")) {
+					form.addClass("invalid");
+				}
+			} else {
+				emailError.css("display", "none");
+				if (form.hasClass("invalid")) {
+					form.removeClass("invalid");
+				}
+			}
+		});
+		contactForm.subject.addEventListener("input", function (event) {
+			if (contactForm.subject.validity.patternMismatch) {
+				subjectError.css("display", "");
+				if (!form.hasClass("invalid")) {
+					form.addClass("invalid");
+				}
+			} else {
+				subjectError.css("display", "none");
+				if (form.hasClass("invalid")) {
+					form.removeClass("invalid");
+				}
+			}
+		});
+		contactForm.message.addEventListener("input", function (event) {
+			if (contactForm.message.value.length > 0 && contactForm.message.value.length < 5) {
+				messageError.css("display", "");
+				if (!form.hasClass("invalid")) {
+					form.addClass("invalid");
+				}
+			} else {
+				messageError.css("display", "none");
+				if (form.hasClass("invalid")) {
+					form.removeClass("invalid");
+				}
+			}
+		});
+	};
+	// add event input listeners
+	addInputEvtListeners();
 
 	form.submit(function (event) {
 		// Stop the browser from submitting the form.
 		event.preventDefault();
 
-		// Clear error fields
-		var error = false;
-		nameError.css('display', 'none');
+		// Validate (name, subject and message) inputs
+		validateTextInputs();
 
-		// Validate input
-		if (!form[0].alias.validity.valid) {
-			nameError.css('display', '');
-			error = true;
-		}
+		// implement this
+		validateHiddenInputs();
 
-		if (error == true)
-			return;
 		// Serialize the form data.
 		var formData = {};
 		formData["Name"] = form[0].alias.value;
@@ -43,4 +116,28 @@ $(function () {
 		//console.log(formData);
 	});
 	// TODO: The rest of the code will go here...
+
+
+	var validateTextInputs = function () {
+		var trimmedName = contactForm.alias.value.trim();
+		if (trimmedName.length < 5) {
+			contactForm.alias.value = trimmedName;
+			nameError.css("display", "");
+			return;
+		}
+		var trimmedSubj = contactForm.subject.value.trim();
+		if (trimmedSubj.length < 5) {
+			contactForm.subject.value = trimmedSubj;
+			subjectError.css("display", "");
+			return;
+		}
+		var trimmedMess = contactForm.message.value.trim();
+		if (trimmedMess.length < 5) {
+			contactForm.message.value = trimmedMess;
+			messageError.css("display", "");
+			return;
+		}
+	};
+
+	var validateHiddenInputs = function () { };
 });
