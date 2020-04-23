d8d9cd7fd90ccc00882e4c9c169392cd0fd06fc9
freelance <freelance-computing@outlook.com>
2020-04-23 17:16:36 +0100

    implemented exception handling inphp mailer

--- demoRainmakersEnts/post.php
+++ demoRainmakersEnts/post.php
@@ -1,22 +1,26 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 require_once 'PHPMailer/src/PHPMailer.php';
 require_once 'PHPMailer/src/SMTP.php';
+require_once 'PHPMailer/src/Exception.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    // Grab form info var_dump($_POST);
-	$name = trim($_POST["Name"]);
-	$recipient = "geanyb2713@gmail.com";
-	$subject = "New contact request from $name";
-	$email_content = "My long awaited email :D";
+    // Grab form info //var_dump($_POST);
+	$name = $_POST["Name"];
+	$number = $_POST["Number"];
+	$sender = $_POST["Email"];
+	$subject = $_POST["Subject"];
+	$email_content = $_POST["Message"];
+
 	$admin = "contact@rainmakersents.co.uk";
-	$sender = "freelance-computing@outlook.com";
+	$recipient = "geanyb2713@gmail.com";
 
 	// Configure mail client
-	$mail = new PHPMailer();
+	$mail = new PHPMailer(true);
 	$mail->isSMTP();
 	$mail->Host = "server242.web-hosting.com";
 	$mail->SMTPAuth = true;
@@ -31,15 +35,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $mail->Body    = $email_content;
 
 	// Send the email
-	if($mail->send())
+	try
 	{
+	    $mail->send();
         http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } 
-	else
+        //echo "Thank You! Your message has been sent.";
+    }
+	catch(Exception $e)
 	{
         http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message. \nError: " .$mail->ErrorInfo;
+        echo "\nOops! Something went wrong and we couldn't send your message. \nError: " .$mail->ErrorInfo;
     }
 }
 else
