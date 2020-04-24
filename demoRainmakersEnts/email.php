793072f43b0c5598b06853ccf6b3cbd8d61f47e7
freelance <freelance-computing@outlook.com>
2020-04-24 04:27:12 +0100

    deleted custom fonts; adjusted borders

--- demoRainmakersEnts/email.php
+++ demoRainmakersEnts/email.php
@@ -2,40 +2,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
     <style>
         *,
         ::after,
         ::before {
             box-sizing: border-box;
         }
-        /*@font-face {
-            font-family: 'supergroteska';
-            font-style: normal;
-			font-weight: normal;
-            src: local('supergroteska'), url(/supergroteska/supergroteska-rg-webfont.woff) format('woff');            
-        }
-		
-		@font-face {
-		  font-family: 'Raleway';
-		  font-style: normal;
-		  font-weight: 400;
-		  src: local('Raleway'), local('Raleway-Regular'), url(https://fonts.gstatic.com/s/raleway/v14/1Ptug8zYS_SKggPNyCMIT5lu.woff2) format('woff2');
-		  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-		}
-		
-		@font-face {
-		  font-family: 'Raleway';
-		  font-style: normal;
-		  font-weight: 400;
-		  src: local('Raleway'), local('Raleway-Regular'), url(https://fonts.gstatic.com/s/raleway/v14/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-		  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-		}*/
         html, body {
             height: 100%;
         }
         body {
-            font-family: 'Raleway', sans-serif;
+            font-family: consolas, sans-serif;
             font-size: 18px;
             font-weight: normal;
             line-height: 1.8;
@@ -61,7 +38,7 @@
         .ts-speaker-popup {
             background: #fff;
             padding: 0;
-            position: relative
+            position: relative;
         }
         .container {
             width: 80%;
@@ -84,27 +61,27 @@
             -ms-flex: 0 0 50%;
             flex: 0 0 50%;
             width: 50%;
-            max-width: 50%
+            max-width: 50%;
         }
         .col-8 {
             -webkit-box-flex: 0;
             -ms-flex: 0 0 66%;
             flex: 0 0 66%;
             width: 66%;
-            max-width: 66%
+            max-width: 66%;
         }
         .ts-speaker-popup .ts-speaker-popup-content {
-            padding: 60px 40px
+            padding: 60px 40px;
         }
 
             .ts-speaker-popup .ts-speaker-popup-content .ts-title {
-                color: #1c1c24
+                color: #1c1c24;
             }
 
             .ts-speaker-popup .ts-speaker-popup-content .speaker-designation {
                 display: block;
                 margin-bottom: 20px;
-                padding-top: 3px
+                padding-top: 3px;
             }
 
         .inner-row {
@@ -113,23 +90,28 @@
             display: flex;
             -ms-flex-wrap: wrap;
             flex-wrap: wrap;
+        }
+        .upper-row {
             border-top: 2px solid #f7f7f7;
+            border-bottom: 2px solid #f7f7f7;
+        }
+        .lower-row {
             border-bottom: 2px solid #f7f7f7;
         }
         @media(max-width:1024px) {
             .container {
-                width: 100%
+                width: 100%;
             }
             .row {
                 margin-right: 0;
-                margin-left: 0
+                margin-left: 0;
             }
             .col-md-11 {
                 -webkit-box-flex: 0;
                 -ms-flex: 0 0 92%;
                 flex: 0 0 92%;
                 width: 92%;
-                max-width: 92%
+                max-width: 92%;
             }
         }
         @media(max-width:768px), (max-width:991px){
@@ -139,17 +121,17 @@
             }
             .row {
                 margin-right: 0;
-                margin-left: 0
+                margin-left: 0;
             }
             .col-xs-12 {
                 -webkit-box-flex: 0;
                 -ms-flex: 0 0 100%;
                 flex: 0 0 100%;
                 width: 100%;
-                max-width: 100%
+                max-width: 100%;
             }
             .ts-speaker-popup .ts-speaker-popup-content {
-                padding: 50px 30px
+                padding: 50px 30px;
             }
         }
     </style>
@@ -161,8 +143,11 @@
                 <div class="col-8 col-md-11 col-xs-12">
                     <div class="ts-speaker-popup-content">
                         <h3 class="ts-title"><?= $name ?></h3>
-                        <div class="inner-row">
+                        <div class="inner-row upper-row">
                             <div class="col-6 col-xs-12"><span class="speaker-designation"><?= $number ?></span></div>
+                        </div>
+						<p></p>
+                        <div class="inner-row lower-row">
                             <div class="col-6 col-xs-12"><span class="speaker-designation"><?= $sender ?></span></div>
                         </div>
                         <p></p>
