02ced9aad9fc54d4aa71a362f726ebb8e79ae50c
freelance <freelance-computing@outlook.com>
2020-04-24 03:41:06 +0100

    uncommented viewport and saved as utf-8 without BOM

--- demoRainmakersEnts/email.php
+++ demoRainmakersEnts/email.php
@@ -1,7 +1,7 @@
-﻿<html>
+<html>
 <head>
     <meta charset="UTF-8">
-    <!--meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
     <style>
         *,
