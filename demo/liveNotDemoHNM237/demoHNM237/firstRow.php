21504cd936da5676f8a189f34601c4f56ecb87f7
freelance <freelance-computing@outlook.com>
2020-08-18 21:09:06 +0100

    show a blank page if viewing the template and not one of its cloned pages

--- demo/liveNotDemoHNM237/demoHNM237/firstRow.php
+++ demo/liveNotDemoHNM237/demoHNM237/firstRow.php
@@ -3,8 +3,10 @@
     <cms:editable name='full_name' label='Full Name' desc='Enter the full name here' type='text'/>
     <cms:editable name='role' label='Role' desc='Enter the role or position here' type='text'/>
     <cms:editable name='description' label='Description' desc='Enter the description here' type='text'/>
+    <cms:editable name='picture_description' label='Picture Description' desc='Enter a short image description here' type='text'/>
     <cms:editable name='picture' label='Picture' desc='Upload the picture here' crop='1' width='500' height='408' type='image'/>
 </cms:template>
+<cms:if k_is_page >
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,7 +24,7 @@
 
         <div class="w3-col l6 m6 w3-margin-bottom">
             <div class="w3-card">
-                <img src="<cms:show picture />" alt="Persis" style="width:100%">
+                <img src="<cms:show picture />" alt="<cms:show picture_description />" style="width:100%">
                 <div class="w3-container">
                     <h3><cms:show full_name /></h3>
                     <p class="w3-opacity"><cms:show role /></p>
@@ -34,4 +36,5 @@
 
 </body>
 </html>
+</cms:if>
 <?php COUCH::invoke(); ?>
