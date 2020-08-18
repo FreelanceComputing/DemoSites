fc16c46014f3fb4b384cf34e0635c5778c403d01
freelance <freelance-computing@outlook.com>
2020-08-18 19:37:01 +0100

    added dummy template for the items on the first row

---
+++ demo/liveNotDemoHNM237/demoHNM237/firstRow.php
@@ -0,0 +1,31 @@
+﻿<?php require_once( 'the-digital-bee/cms.php' ); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hidden No More Cameroon</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/raleway.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/functions.js"></script>
+</head>
+<body>
+
+
+        <div class="w3-col l6 m6 w3-margin-bottom">
+            <div class="w3-card">
+                <img src="img/members/1persis.jpg" alt="Persis" style="width:100%">
+                <div class="w3-container">
+                    <h3>Persis Mbangsi</h3>
+                    <p class="w3-opacity">President</p>
+                    <p>Persis received her Bachelor’s degree with honors and Master’s Degree in Chemical Engineering at the University of Cape Town, South Africa. Persis has 6 years’ work experience then as Process and Quality Engineer and now the Process Supervisor in the Reduction Unit of ALUCAM, Cameroon.</p>
+                </div>
+            </div>
+        </div>
+
+
+</body>
+</html>
+<?php COUCH::invoke(); ?>
